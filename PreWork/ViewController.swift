de7365a5c16f3153242039e13db11f32d4f15799
Lehan Ouyang <lo739@nyu.edu>
2022-12-26 16:25:49 -0500

    Initial Commit

---
+++ PreWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreWork
+//
+//  Created by lehan ouyang on 12/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
