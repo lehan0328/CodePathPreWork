ceeac617a08aad222563de75e8860c3347b6d998
Lehan Ouyang <lo739@nyu.edu>
2022-12-26 22:32:24 -0500

    define outlet

--- PreWork/ViewController.swift
+++ PreWork/ViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNameTestField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var numberOfPetLabel: UILabel!
+    @IBOutlet weak var morePetStepper: UIStepper!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
